9b94a626106737a4727020019e90000497f031f7
Patri <pmartinperez@danielcastelao.org>
2016-01-29 13:55:18 +0100

    Configurando el fichero openerp

--- ChachiTema/__openerp__.py
+++ ChachiTema/__openerp__.py
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>TODO write content</div>
-    </body>
-</html>
+{
+  'name':'Chachi tema',
+  'description': 'Un tema muy chachi',
+  'version':'1.0',
+  'author':'Patripon',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_less'],
+}
